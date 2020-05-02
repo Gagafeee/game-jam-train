b37d651fe1319f4f58010cf4f83e5460237b52c9
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2020-05-02 17:39:12 +0200

    Menu1 update

---
+++ GameJam/Assets/MainMenuScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public string SceneManager;
+    public string sceneName;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+      SceneManager.LoadScene(sceneName);
+    }
+}
