915bb50b0d7a7ad5887d96b8d44697c7588e1b59
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2020-05-03 11:40:29 +0200

    Menu Update

--- GameJam/Assets/MainMenuScript.cs
+++ GameJam/Assets/MainMenuScript.cs
@@ -4,11 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public string SceneManager;
     public string sceneName;
-    
-    private void OnCollisionEnter2D(Collision2D collision)
+
+     public void LoadGame()
+        {
+          SceneManager.LoadScene(sceneName);
+        }
+
+    public void stopTheGame()
     {
-      SceneManager.LoadScene(sceneName);
+        Application.Quit();
     }
 }
